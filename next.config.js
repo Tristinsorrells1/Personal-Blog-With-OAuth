001b2a3046252326a6b1acff7fcfc9790fba44d0
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-04-17 16:34:57 -0500

    Deploy without lint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
+};
