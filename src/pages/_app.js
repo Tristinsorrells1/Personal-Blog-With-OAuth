8423ae762a64e0e9e8b3ca03332a2f560fa72c4c
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-04-17 16:44:49 -0500

    Add viewport meta tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,13 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import Head from 'next/head'
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
